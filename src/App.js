d2f6146632ca87099bde2cb5193eb4d0cf84e9fe
Fernanda Schneider <veludoudigrudi@gmail.com>
2023-02-23 08:07:10 -0300

    requisito 2 completo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './pages/Search';
 import Login from './pages/Login';
 import Album from './pages/Album';
@@ -12,15 +12,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      isSaveButtonDisabled: true,
+      isLoading: false,
+    };
+  }
+
+  // aciona a validação do botão Entrar (req 2)
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.setState({ isSaveButtonDisabled: this.validateButton() });
+    });
+  };
+
+  // faz com que o botão Entrar só funcione depois de 3 caracteres (req 2)
+  validateButton = () => {
+    const { name } = this.state;
+    const minNum = 3;
+    const nameButton = name.length < minNum;
+    return nameButton;
+  };
+
+  // func acionada ao clicar o botão Entrar (req 2)
+  handleClick = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    history.push('/search');
+  };
+
   render() {
+    const {
+      name,
+      isSaveButtonDisabled,
+      isLoading,
+    } = this.state;
+
+    if (isLoading) return <Loading />;
+
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <label htmlFor="login-input">
+          Nome
+          <input
+            data-testid="login-name-input"
+            type="text"
+            id="login-input"
+            name="name"
+            value={ name }
+            onChange={ this.onInputChange }
+          />
+        </label>
+        <button
+          data-testid="login-submit-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.func.isRequired,
+};
 
 export default Login;
