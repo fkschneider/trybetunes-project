7fff9793d2f29fe7dfa7900662128ae5477bb56a
Fernanda Schneider <veludoudigrudi@gmail.com>
2023-02-24 14:07:47 -0300

    requisitos 6 e 7 não finalizados

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      songs: [],
+      artist: '',
+      album: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getAlbum()
+  }
+
+  getAlbum = async () => {
+    const { songs } = this.state;
+    const { match: { params: { id } } } = this.props;
+    const fullAlbum = await getMusics(id);
+    this.setState({ 
+      songs: fullAlbum, 
+      // artist: songs[0].artistName, 
+      // album: songs[0].albumName 
+    });
+    console.log(songs);
+  }
+
   render() {
+    const { artist, album } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <div>
+          <p data-testid="artist-name">{artist}</p>
+          <p data-testid="album-name">{album}</p>
+        </div>
       </div>
     );
   }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,45 +1,89 @@
 import React from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
+import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       isSearchButtonDisabled: true,
+      isLoading: false,
+      inputArtist: '',
+      searchedAlbums: [],
     };
   }
 
+  // validação do botão Pesquisar/atualizar estado inputArtist (req 5 e 6)
   handleChange = ({ target }) => {
     const { value } = target;
-    // console.log(value);
+    this.setState({ inputArtist: value })
     if (value.length >= 2) {
       this.setState({ isSearchButtonDisabled: false });
     }
   };
 
+  // busca álbuns pelo artista (req 6)
+  handleAlbums = async () => {
+    const { inputArtist } = this.state;
+    const { searchedAlbums } = this.state;
+    this.setState({ isLoading: true });
+    const searchAlbumsByArtist = await searchAlbumsAPI(inputArtist);
+    this.setState({ isLoading: false }, () => {
+      this.setState({ searchedAlbums: searchAlbumsByArtist });
+    });
+    // console.log(searchAlbumsByArtist);
+  }
+
   render() {
     const {
       isSearchButtonDisabled,
+      isLoading,
+      searchedAlbums,
+      inputArtist,
     } = this.state;
+
+    if (isLoading) return <Loading />;
 
     return (
       <div data-testid="page-search">
         <Header />
-        <label htmlFor="search-input">
+        <label htmlFor="inputArtist">
           <input
             type="text"
-            name="search-input"
+            name="inputArtist"
+            value={inputArtist}
             data-testid="search-artist-input"
-            onChange={ this.handleChange }
+            onChange={this.handleChange}
           />
         </label>
         <button
           type="button"
           data-testid="search-artist-button"
-          disabled={ isSearchButtonDisabled }
+          disabled={isSearchButtonDisabled}
+          onClick={this.handleAlbums}
         >
           Pesquisar
         </button>
+        <section>
+          {searchedAlbums.length === 0 ? <p>Nenhum álbum foi encontrado</p> :
+          <p>Resultado de álbuns de: {inputArtist}</p>}
+        </section>
+        <div>
+          <ul>
+            {searchedAlbums.map((album) => (
+              <Link to={`/album/${album.collectionId}`}
+                data-testid={`link-to-album-${album.collectionId}`}>
+                <li
+                  key={album.collectionId}
+                >
+                  {album.collectionName}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
